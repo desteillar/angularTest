bc451a8d150bc814461eb242df343c866667c60e
desteillar <53055248+desteillar@users.noreply.github.com>
2021-04-17 14:57:30 +0200

    adding comment for code quality

--- src/app/components/data-process-component/data-process-component.component.ts
+++ src/app/components/data-process-component/data-process-component.component.ts
@@ -14,6 +14,7 @@ export class DataProcessComponentComponent implements OnInit {
   title = new FormControl('');
   constructor(private getData: GetDataService) { }
 
+  //* Initialization of table data with the result of json provided by the method getAllData */
   ngOnInit(): void {
     this.getData.getAllData().subscribe(
       resp => this.allData = resp,
@@ -21,15 +22,18 @@ export class DataProcessComponentComponent implements OnInit {
     );
   }
 
+  //*Method of adding an element to the json  */
   addAtodo() {
     const body = { id: null, title : this.title.value, userId : 1, completed : false}
     this.getData.postData(body).subscribe(data => {
       this.allData.push(data)
     })
     this.title.reset();
-    
+
 
   }
+
+  //*This method serves to check the button display all or completed based on that it redirect to the required method  */
   checkButtonDisplay() {
     if (this.buttonDisplay === "Display all") {
       this.getDataByBool()
@@ -38,6 +42,7 @@ export class DataProcessComponentComponent implements OnInit {
     }
   }
 
+   //*This method serves to get all data  */
   getAlldata() {
     this.buttonDisplay = "Display all"
     this.getData.getAllData().subscribe(
@@ -45,7 +50,7 @@ export class DataProcessComponentComponent implements OnInit {
       error => console.log('Attention: il y a une erreur' + error)
     );
   }
-
+//*This method serves to get data by his boolean*/
   getDataByBool() {
     var arrayTemp = this.allData
     this.allData = []
--- src/app/services/get-data.service.ts
+++ src/app/services/get-data.service.ts
@@ -9,11 +9,11 @@ export class GetDataService {
   jsonUrl="https://jsonplaceholder.typicode.com/todos?_start=0&_limit=10";
   postJsonURl = "https://jsonplaceholder.typicode.com/todos";
   constructor(private http: HttpClient) { }
-
+  //*This method serve to create a http link between the json file and the front side. This is a get method */
   public getAllData():Observable<any>{
     return this.http.get<any>(this.jsonUrl);
   }
-
+ //*This method serve to create a http link between the json file and the front side. This is a POST method*/
   public postData(body:Object): Observable<any>{
     return this.http.post<any>(this.postJsonURl,body);
   }
